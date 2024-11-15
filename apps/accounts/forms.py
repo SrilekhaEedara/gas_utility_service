bc2014064f47cb655180ed8ccbb8a9db23349e43
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 16:13:10 +0530

    Update forms.py

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,1 +1,8 @@
+# apps/accounts/forms.py
+from django import forms
 
+class UserProfileForm(forms.Form):
+    # Example fields
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
