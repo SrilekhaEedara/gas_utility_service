2a59d27231af03b0c5faa52e099219fe595b5c68
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 14:02:00 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,1 +1,11 @@
+# config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('apps.accounts.urls')),
+    path('requests/', include('apps.requests.urls')),
+    path('support/', include('apps.support.urls')),
+]
 
