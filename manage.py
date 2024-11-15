5ebd90cf2fbecd8c3cadd369b98487b8ae7b3ce1
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 14:20:37 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # This will fail if Django is not installed properly.
+        raise ImportError(
+            "Could not import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? "
+            "Did you forget to activate a virtual environment?"
+        )
+
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
 
