a4019fe797f520fa02e99f171696c9210137a617
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 14:02:35 +0530

    Update models.py

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,1 +1,13 @@
+# apps/accounts/models.py
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
 
