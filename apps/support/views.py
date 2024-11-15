d314eb93b2fa22fdbce56be561fd3d2cc6ae7705
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 14:16:48 +0530

    Update views.py

--- apps/support/views.py
+++ apps/support/views.py
@@ -1,1 +1,9 @@
+# apps/support/views.py
+
+from django.shortcuts import render
+from .models import ServiceRequest, SupportAction
+
+def manage_requests(request):
+    requests = ServiceRequest.objects.filter(status='Pending')
+    return render(request, 'support/manage_requests.html', {'requests': requests})
 
