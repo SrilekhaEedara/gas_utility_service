048f4ee26f7ce59b010dee57a3229c56cfa70c16
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 14:16:01 +0530

    Update views.py

--- apps/requests/views.py
+++ apps/requests/views.py
@@ -1,1 +1,20 @@
+# apps/requests/views.py
+
+from django.shortcuts import render
+from .forms import ServiceRequestForm
+from .models import ServiceRequest
+
+def create_request(request):
+    if request.method == 'POST':
+        form = ServiceRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('track_request')
+    else:
+        form = ServiceRequestForm()
+    return render(request, 'requests/create_request.html', {'form': form})
+
+def track_request(request):
+    service_requests = ServiceRequest.objects.filter(customer=request.user.profile)
+    return render(request, 'requests/track_request.html', {'requests': service_requests})
 
