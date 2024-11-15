f0b0e3ede987d90a611b4e9753d1457841afc3bc
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 16:10:53 +0530

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),  # Corrected function name
+    path('login/', views.login_view, name='login'),           # Corrected function name
+]
 
