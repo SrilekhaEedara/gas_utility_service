70c58a7e06c73d27ea94d6bf4769c7b92ba1ca9f
Srilekha Eedara <149938571+SrilekhaEedara@users.noreply.github.com>
2024-11-15 13:44:49 +0530

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-# Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15)
